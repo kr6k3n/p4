82742fcb9a1224c0664de8c9d019b6dd4855699a
Solal <s.baudoin21@ejm.org>
2021-04-09 19:45:46 +0200

    sauvegarde progrès

--- src/learning/trainpool.py
+++ src/learning/trainpool.py
@@ -16,8 +16,7 @@ class TrainPool:
 		self.name = name
 		print("Creating", self.name, "generation")
 		print(f"Creating initial {agent.name} population...")
-		self.population: List[Agent] = fastmap(new_agent, range(population_size),
-																					              display_progress=True)
+		self.population: List[Agent] = fastmap(new_agent, range(population_size),display_progress=True)
 	
 	def compete(self) -> None:
 		# add multiprocessing here later for sped
@@ -41,73 +40,12 @@ class TrainPool:
 			os.mkdir(pool_path)
 		except FileExistsError:
 			pass
-		
-		
-
-	"""def next_gen(self) -> None:
-		#sort by score
-		sorted_snakes = sorted(
-			self.population, key=lambda s: s.score(), reverse=True)
-		print("best score here", sorted_snakes[0].score())
-		# kill all snakes except top 25%
-		print("killing snakes...")
-		sorted_snakes = sorted_snakes[:self.population_size//4]
-		print("merging snakes")
-		new_snakes_amount = max(1, self.population_size//100)
-		new_children_amount = self.population_size-new_snakes_amount
-		self.population = fastmap(create_child, list(
-			sorted_snakes for _ in range(new_children_amount)), display_progress=True)
-		print("adding new children")
-		self.population += fastmap(new_snake,
-								   range(new_snakes_amount), display_progress=True)"""
-
-
-def now_file_name() -> str:
-    now = datetime.datetime.now()
-    return f"{BASE_DIR}/account_data/{now.day}-{now.month}-{now.year} {now.hour}:{now.minute}:{now.second}"
-
-
-def store_accounts(accounts: List[Instagram_Account], latest_data=LATEST_DATA, now_data=now_file_name()) -> None:
-    """
-        Dumps account data into both "latest" file and current date file.
-    """
-    latest = open(latest_data, "wb")
-    now = open(now_data, "wb")
-
-    pickle.dump(accounts, latest)
-    pickle.dump(accounts, now)
-    latest.close()
-    now.close()
-
-
-def get_accounts(latest_data=LATEST_DATA) -> List[Instagram_Account]:
-    """
-        Gets account data from "latest" file.
-    """
-    latest = open(latest_data, "rb")
-    accounts = pickle.load(latest)
-    latest.close()
-    return accounts
-
-
-def add_accounts(accounts: List[Instagram_Account], lastest_data=LATEST_DATA):
-    new_accounts = None
-    try:
-        new_accounts = get_accounts()
-    except FileNotFoundError:
-        new_accounts = []
-    new_accounts.extend(accounts)
-    store_accounts(new_accounts)
-
-
-def display_accounts() -> None:
-    try:
-        accounts = get_accounts()
-    except FileNotFoundError:
-        print("Can't display accounts: no storage created yet : (")
-    account_table: List[List[Any]] = [
-    	["Username", "Password", "Tags", "Reddit Threads"]]
-    for account in accounts:
-        account_table.append([account.username, account.password, len(
-            account.tags), account.fetcher_description["Reddit"]])
-    print(tabulate(account_table, headers="firstrow"))
+		now = datetime.datetime.now()
+		now_path =  f"{pool_path}/{now.day}-{now.month}-{now.year} {now.hour}:{now.minute}:{now.second}"
+		latest_path = f"{pool_path}/latest"
+		now_file = open(now_path, "wb")
+		latest_file = open(latest_path, "wb")
+		pickle.dump(self, now_file)
+		pickle.dump(self, latest_file)
+		now_file.close()
+		latest_file.close()
